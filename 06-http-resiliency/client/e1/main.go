b5352936e367df2e852fe7a1c1e6d1bf132f30dc
Johnny Boursiquot <jboursiquot@gmail.com>
2022-04-06 09:46:36 -0400

    Use helper for new request with context

--- 06-http-resiliency/client/e1/main.go
+++ 06-http-resiliency/client/e1/main.go
@@ -26,8 +26,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
-	req, _ := http.NewRequest(http.MethodGet, ts.URL, nil)
-	req = req.WithContext(ctx)
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, ts.URL, nil)
 
 	t := time.Now()
 	log.Println("Sending request...")
